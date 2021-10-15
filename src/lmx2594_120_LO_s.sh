ce4c1b6666fe818a12dca6312345064f80323ed2
Takefumi MIYOSHI <miyo@wasamon.net>
2021-10-15 19:08:55 +0900

    add lmx2594_120_LO_s.sh to config only a LMX2594

---
+++ src/lmx2594_120_LO_s.sh
@@ -0,0 +1,83 @@
+./udpsendrecv lmx2594 w $1 00 2612 # R6 [14]VCO_PHASE_SYNC=0, [9]OUT_MUTE=1
+./udpsendrecv lmx2594 w $1 00 2610
+./udpsendrecv lmx2594 r $1 00 0
+./udpsendrecv lmx2594 w $1 4E 0001
+./udpsendrecv lmx2594 w $1 4D 0000
+./udpsendrecv lmx2594 w $1 4C 000C
+./udpsendrecv lmx2594 w $1 4B 0840
+./udpsendrecv lmx2594 w $1 4A 0000 # R74 [15:2] SYSREF_PULSE_COUNT=0
+./udpsendrecv lmx2594 w $1 49 003F
+./udpsendrecv lmx2594 w $1 48 0000 # R72 [10:0] SYSREF_DIV
+./udpsendrecv lmx2593 w $1 47 0081 # R71 [7:5]SYSREF_DIV_PRE=4='Divided by 4', [3]SYSREF_EN=0, [2]SYSREF_REPEAT=0
+./udpsendrecv lmx2594 w $1 46 C350
+./udpsendrecv lmx2594 w $1 45 0000
+./udpsendrecv lmx2594 w $1 44 03E8
+./udpsendrecv lmx2594 w $1 43 0000
+./udpsendrecv lmx2594 w $1 42 01F4
+./udpsendrecv lmx2594 w $1 41 0000
+./udpsendrecv lmx2594 w $1 40 1388
+./udpsendrecv lmx2594 w $1 3F 0000
+./udpsendrecv lmx2594 w $1 3E 0322
+./udpsendrecv lmx2594 w $1 3D 00A8
+./udpsendrecv lmx2594 w $1 3C 03E8
+./udpsendrecv lmx2594 w $1 3B 0001
+./udpsendrecv lmx2594 w $1 3A 8001 # R58 [15]IGNORE=0, [14]HYST=0, [13:12]INPIN_LVL=0=Vin/4, [11:9]FMT=2='sysrefreq=LVDS'
+./udpsendrecv lmx2594 w $1 39 0020
+./udpsendrecv lmx2594 w $1 38 0000
+./udpsendrecv lmx2594 w $1 37 0000
+./udpsendrecv lmx2594 w $1 36 0000
+./udpsendrecv lmx2594 w $1 35 0000
+./udpsendrecv lmx2594 w $1 34 0820
+./udpsendrecv lmx2594 w $1 33 0080
+./udpsendrecv lmx2594 w $1 32 0000
+./udpsendrecv lmx2594 w $1 31 4180
+./udpsendrecv lmx2594 w $1 30 0300
+./udpsendrecv lmx2594 w $1 2F 0300
+./udpsendrecv lmx2594 w $1 2E 07FD # R46 [1:0] OUTB_MUX=1(=VCO)
+./udpsendrecv lmx2594 w $1 2D C8DF # R45 [5:0] OUTB_PWR=1F
+./udpsendrecv lmx2594 w $1 2C 3220 # R44
+./udpsendrecv lmx2594 w $1 2B 0000
+./udpsendrecv lmx2594 w $1 2A 0000
+./udpsendrecv lmx2594 w $1 29 0000
+./udpsendrecv lmx2594 w $1 28 0000
+./udpsendrecv lmx2594 w $1 27 0001
+./udpsendrecv lmx2594 w $1 26 0000
+./udpsendrecv lmx2594 w $1 25 0204
+./udpsendrecv lmx2594 w $1 24 0078
+./udpsendrecv lmx2594 w $1 23 0004
+./udpsendrecv lmx2594 w $1 22 0000
+./udpsendrecv lmx2594 w $1 21 1E21
+./udpsendrecv lmx2594 w $1 20 0393
+./udpsendrecv lmx2594 w $1 1F 43EC
+./udpsendrecv lmx2594 w $1 1E 318C
+./udpsendrecv lmx2594 w $1 1D 318C
+./udpsendrecv lmx2594 w $1 1C 0488
+./udpsendrecv lmx2594 w $1 1B 0002
+./udpsendrecv lmx2594 w $1 1A 0DB0
+./udpsendrecv lmx2594 w $1 19 0C2B
+./udpsendrecv lmx2594 w $1 18 071A
+./udpsendrecv lmx2594 w $1 17 007C
+./udpsendrecv lmx2594 w $1 16 0001
+./udpsendrecv lmx2594 w $1 15 0401
+./udpsendrecv lmx2594 w $1 14 C848
+./udpsendrecv lmx2594 w $1 13 27B7
+./udpsendrecv lmx2594 w $1 12 0064
+./udpsendrecv lmx2594 w $1 11 00FA
+./udpsendrecv lmx2594 w $1 10 0080
+./udpsendrecv lmx2594 w $1 0F 064F
+./udpsendrecv lmx2594 w $1 0E 1E10
+./udpsendrecv lmx2594 w $1 0D 4000
+./udpsendrecv lmx2594 w $1 0C 5001
+./udpsendrecv lmx2594 w $1 0B 0018
+./udpsendrecv lmx2594 w $1 0A 10D8
+./udpsendrecv lmx2594 w $1 09 0604
+./udpsendrecv lmx2594 w $1 08 2000
+./udpsendrecv lmx2594 w $1 07 00B2
+./udpsendrecv lmx2594 w $1 06 C802
+./udpsendrecv lmx2594 w $1 05 00C8
+./udpsendrecv lmx2594 w $1 04 1B43
+./udpsendrecv lmx2594 w $1 03 0642
+./udpsendrecv lmx2594 w $1 02 0500
+./udpsendrecv lmx2594 w $1 01 080B
+./udpsendrecv lmx2594 w $1 00 2618
+./udpsendrecv lmx2594 r $1 00 0
