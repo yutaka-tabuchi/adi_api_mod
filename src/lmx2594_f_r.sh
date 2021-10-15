82570718b9037f5025ab923b6f13ff63eda2d024
Takefumi MIYOSHI <miyo@wasamon.net>
2021-10-15 19:12:40 +0900

    refactoring utility scripts

--- src/lmx2594_f_r.sh
+++ src/lmx2594_f_r.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-for i in 0 1 2 3 4 5 6 7
-do
-	./udpsendrecv lmx2594 r $i 22 0 # R34 [2:0] PLL_N[18:16]
-	./udpsendrecv lmx2594 r $i 24 0 # R36 PLL_N[15:0]
-	./udpsendrecv lmx2594 r $i 26 0 # R38 PLL_DEN[31:16]
-	./udpsendrecv lmx2594 r $i 27 0 # R39 PLL_DEN[15:0]
-	./udpsendrecv lmx2594 r $i 2A 0 # R42 PLL_NUM[31:16]
-	./udpsendrecv lmx2594 r $i 2B 0 # R43 PLL_NUM[15:0]
-done
+
+./udpsendrecv lmx2594 r $1 22 0 # R34 [2:0] PLL_N[18:16]
+./udpsendrecv lmx2594 r $1 24 0 # R36 PLL_N[15:0]
+./udpsendrecv lmx2594 r $1 26 0 # R38 PLL_DEN[31:16]
+./udpsendrecv lmx2594 r $1 27 0 # R39 PLL_DEN[15:0]
+./udpsendrecv lmx2594 r $1 2A 0 # R42 PLL_NUM[31:16]
+./udpsendrecv lmx2594 r $1 2B 0 # R43 PLL_NUM[15:0]
--- src/lmx2594_f_w.sh
+++ src/lmx2594_f_w.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-for i in 0 1 2 3 4 5 6 7 8 9
-do
-	./udpsendrecv lmx2594 w $i 22 0000 # R34 [2:0] PLL_N[18:16]
-	./udpsendrecv lmx2594 w $i 24 $1 # R36 PLL_N[15:0]
-	./udpsendrecv lmx2594 w $i 26 0000 # R38 PLL_DEN[31:16]
-	./udpsendrecv lmx2594 w $i 27 0064 # R39 PLL_DEN[15:0]
-	./udpsendrecv lmx2594 w $i 2A 0000 # R42 PLL_NUM[31:16]
-	./udpsendrecv lmx2594 w $i 2B 0000 # R43 PLL_NUM[15:0]
-	./udpsendrecv lmx2594 w $i 00 6418
-done
+./udpsendrecv lmx2594 w $1 22 0000 # R34 [2:0] PLL_N[18:16]
+./udpsendrecv lmx2594 w $1 24 $2 # R36 PLL_N[15:0]
+./udpsendrecv lmx2594 w $1 26 0000 # R38 PLL_DEN[31:16]
+./udpsendrecv lmx2594 w $1 27 0064 # R39 PLL_DEN[15:0]
+./udpsendrecv lmx2594 w $1 2A 0000 # R42 PLL_NUM[31:16]
+./udpsendrecv lmx2594 w $1 2B 0000 # R43 PLL_NUM[15:0]
+./udpsendrecv lmx2594 w $1 00 6418
